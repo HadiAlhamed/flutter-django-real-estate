438879dbee2312a07bf111eb4405f365a525d681
Hadi Alhamed <hadialhamed.py@gmail.com>
2025-05-02 01:38:06 +0300

    update myInputField

--- lib/widgets/my_input_field.dart
+++ lib/widgets/my_input_field.dart
@@ -5,37 +5,63 @@ import 'package:real_estate/textstyles/text_colors.dart';
 
 class MyInputField extends StatelessWidget {
   final String hint;
+  final TextEditingController? controller;
   final IconData? prefixIcon;
   final bool? isObsecure;
   final Widget? suffixWidget;
+  final Widget? prefixWidget;
+
+  final BoxDecoration? decoration;
+  final BorderSide? borderSide;
+  final bool? readOnly;
+  final void Function()? ontap;
+  final TextInputType? keyboardType;
   const MyInputField({
     super.key,
     required this.hint,
     this.prefixIcon,
     this.isObsecure,
     this.suffixWidget,
+    this.decoration,
+    this.borderSide,
+    this.controller,
+    this.readOnly,
+    this.ontap,
+    this.prefixWidget, this.keyboardType,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: decoration,
       margin: const EdgeInsets.only(bottom: 20),
       child: TextFormField(
+        // onTapAlwaysCalled: true,
+        onTap: ontap,
+        readOnly: readOnly ?? false,
+        controller: controller,
         obscureText: isObsecure ?? false,
+        keyboardType: keyboardType,
         decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
           suffixIcon: suffixWidget,
           hintText: hint,
-          prefixIcon: Icon(
-            prefixIcon,
-            color: primaryColor,
-          ),
+          prefixIcon: prefixWidget ??
+              (prefixIcon == null
+                  ? null
+                  : Icon(
+                      prefixIcon,
+                      color: primaryColor,
+                    )),
           enabledBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(15),
-            borderSide: BorderSide(color: primaryColorInactive, width: 3),
+            borderSide:
+                borderSide ?? BorderSide(color: primaryColorInactive, width: 3),
           ),
           focusedBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(15),
-            borderSide: BorderSide(color: primaryColor, width: 4),
+            borderSide: borderSide ?? BorderSide(color: primaryColor, width: 4),
           ),
         ),
       ),

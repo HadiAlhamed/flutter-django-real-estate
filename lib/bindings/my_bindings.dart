aac8416d3f449a1c866b32c43b7571bda5e4716f
Hadi Alhamed <hadialhamed.py@gmail.com>
2025-05-02 01:38:24 +0300

    inject controllers

--- lib/bindings/my_bindings.dart
+++ lib/bindings/my_bindings.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:real_estate/controllers/bottom_navigation_bar_controller.dart';
+import 'package:real_estate/controllers/drop_down_controller.dart';
 import 'package:real_estate/controllers/home_page_tab_controller.dart';
 import 'package:real_estate/controllers/login_controller.dart';
 import 'package:real_estate/controllers/signup_controller.dart';
@@ -12,5 +13,6 @@ class MyBindings extends Bindings {
     Get.put(SignupController());
     Get.put(BottomNavigationBarController());
     Get.put(HomePageTabController());
+    Get.put(DropDownController());
   }
 }

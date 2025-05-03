505a45439a7d7c5c8956215ff669064dba37ba11
Hadi Alhamed <hadialhamed.py@gmail.com>
2025-05-03 22:52:29 +0300

    add controller for forgetpasswordController

---
+++ lib/controllers/forget_password_controller.dart
@@ -0,0 +1,21 @@
+import 'package:get/get.dart';
+
+class ForgetPasswordController extends GetxController {
+  bool emailEntered = false;
+  bool codeVerified = false;
+  String email = '';
+  String code = '';
+  void changeEmailFlag(bool value) {
+    emailEntered = value;
+    update(['main']);
+  }
+
+  void changeCodeFlag(bool value) {
+    codeVerified = value;
+    update(['image', 'main']);
+  }
+
+  set emailValue(String e) => email = e;
+
+  set codeValue(String c) => code = c;
+}

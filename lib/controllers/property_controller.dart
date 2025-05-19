5f31aa18a022334280c634de9dd1315694d7b979
Hadi Alhamed <hadialhamed.py@gmail.com>
2025-05-19 23:09:08 +0300

    add getProperties api

--- lib/controllers/property_controller.dart
+++ lib/controllers/property_controller.dart
@@ -5,11 +5,23 @@ import 'package:real_estate/models/property_image.dart';
 
 class PropertyController extends GetxController {
   List<Property> properties = [];
+  List<Property> houses = [];
+  List<Property> flats = [];
+  List<Property> villas = [];
+
   Map<int, List<PropertyImage>> propertyImages = {};
   Map<int, List<Facility>> propertyFacilities = {};
 
   void addProperty(Property property) {
     properties.add(property);
+    if (property.propertyType.toLowerCase() == 'house') {
+      houses.add(property);
+    } else if (property.propertyType.toLowerCase() == 'flat') {
+      flats.add(property);
+    } else {
+      //villa
+      villas.add(property);
+    }
   }
 
 //XFile -> MultiPartFile -> FormData
--- lib/services/properties_apis/properties_apis.dart
+++ lib/services/properties_apis/properties_apis.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:real_estate/models/facility.dart';
+import 'package:real_estate/models/paginated_property.dart';
 import 'package:real_estate/models/property.dart';
 import 'package:real_estate/models/property_image.dart';
 import 'package:real_estate/services/api.dart';
@@ -97,6 +98,30 @@ class PropertiesApis {
     } catch (e) {
       print("Network Error : $e");
       return null;
+    }
+  }
+
+  static Future<PaginatedProperty> getProperty({String? url}) async {
+    print("trying to get property page : $url");
+    try {
+      final response = await _dio.get(
+        url ?? "${Api.baseUrl}/properties/",
+      );
+      if (response.statusCode == 200) {
+        print("Retrived successfully");
+        Map<String, dynamic> data = response.data;
+        List<Property> properties = (data['results'] as List).map((property) {
+          return Property.fromJson(property);
+        }).toList();
+        return PaginatedProperty(
+            nextPageUrl: data['next'] as String, properties: properties);
+      } else {
+        print(response.statusMessage);
+        return PaginatedProperty(nextPageUrl: null, properties: []);
+      }
+    } catch (e) {
+      print("Network Error : $e");
+      return PaginatedProperty(nextPageUrl: null, properties: []);
     }
   }
 }

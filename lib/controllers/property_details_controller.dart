d6cc79aff08b88814c7e5ab07ddf69e65bebe0a3
Hadi Alhamed <hadialhamed.py@gmail.com>
2025-06-17 01:33:46 +0300

    update propertyDetailsController to handle favorties better

--- lib/controllers/property_details_controller.dart
+++ lib/controllers/property_details_controller.dart
@@ -1,17 +1,59 @@
 import 'package:get/get.dart';
+import 'package:real_estate/models/property.dart';
 import 'package:real_estate/models/property_details.dart';
+import 'package:real_estate/services/properties_apis/properties_apis.dart';
 
 class PropertyDetailsController extends GetxController {
   bool isLoading = true;
   PropertyDetails? propertyDetails;
-  List<bool> isFavorite = List.generate(100005, (index)=>false);
+  List<bool> isFavorite = List.generate(100005, (index) => false);
+  Set<Property> isFavoriteSet = {};
   void changeIsLoading(bool value) {
     isLoading = value;
     update(['main']);
   }
 
-  void flipIsFavorite({required int propertyId}) {
+  void flipIsFavorite({required int propertyId,}) {
     isFavorite[propertyId] = !isFavorite[propertyId];
-    update(['isFavorite']);
+    update(['isFavorite', "isFavorite$propertyId"]);
+  }
+
+  void addToFavorites() {
+    Property toAdd = propertyDetails!.toProperty();
+    if (isFavorite[propertyDetails!.id]) {
+      if (!isFavoriteSet.contains(toAdd)) {
+        isFavoriteSet.add(toAdd);
+      }
+    } else {
+      if (!isFavoriteSet.contains(toAdd)) {
+        isFavoriteSet.remove(toAdd);
+      }
+    }
+  }
+
+  void initFavorites(Property property) {
+    isFavoriteSet.add(property);
+    isFavorite[property.id!] = true;
+  }
+
+  @override
+  void onClose() async {
+    // TODO: implement onClose
+    List<Property> toDelete = [];
+    for (Property property in isFavoriteSet) {
+      if (isFavorite[property.id!]) continue;
+      toDelete.add(property);
+    }
+    for (Property property in toDelete) {
+      await PropertiesApis.cancelFavorite(propertyId: property.id!);
+      isFavoriteSet.remove(property);
+    }
+    super.onClose();
+  }
+
+  void clear() {
+    isLoading = true;
+    isFavorite = List.generate(100005, (index) => false);
+    isFavoriteSet = {};
   }
 }
